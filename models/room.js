5dd434de205a1e750b95be80409b1121e1dd1301
salih daher <salihdaher9@gmail.com>
2024-05-13 20:00:54 +0300

    fixed room model

--- models/room.js
+++ models/room.js
@@ -6,6 +6,10 @@ const RoomSchema = new Schema({
   Number: { type: Number, required: true },
   price: { type: Number, required: true },
   pictures: [{ type: String, required: true }],
+  calender:[{
+    Date: { type: Date},
+    empty: { type: Boolean}
+  }]
 });
 
 module.exports = mongoose.model("Room", RoomSchema);
