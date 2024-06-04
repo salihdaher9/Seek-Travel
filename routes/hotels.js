9c0ac0a84ee4b33bcc4b38e73b16dd8d9ce1cda9
naseemhamze <naseem.raef@gmail.com>
2024-06-04 18:33:02 +0300

    changing review from range to stars

--- routes/hotels.js
+++ routes/hotels.js
@@ -6,28 +6,19 @@ const ValidateHotelSchema = require("../utils/VlaidateMiddlewear"); //hotel sche
 const { isLoggedIn } = require('../utils/Authorizationmiddleware');
 const { isOwner } = require("../utils/Authorizationmiddleware");
 
-const ExpressError = require('../utils/ExpressError');
-const Hotel = require('../models/hotel')
 
 router.get("/", hotels.index);
 
-
 router.get("/new", isLoggedIn, WrapAsync(hotels.renderNewForm));
-
 
 router.post("/", isLoggedIn, ValidateHotelSchema, WrapAsync(hotels.createHotelasync));
 
-
 router.get("/:id", WrapAsync(hotels.showHotel));
-
 
 router.get("/:id/edit", isLoggedIn, isOwner, WrapAsync(hotels.renderEditForm));
 
-
 router.put("/:id", isLoggedIn, isOwner, ValidateHotelSchema, WrapAsync(hotels.updateHotel));
 
-
 router.delete("/:id", isLoggedIn, isOwner, WrapAsync(hotels.deleteHotel));
-
 
 module.exports = router;
