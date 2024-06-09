3c89587be654e26da269714aea25db78065c7355
naseemhamze <naseem.raef@gmail.com>
2024-06-09 20:31:02 +0300

    adding one map to all hotels in home page and mas to each hotel

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -1,5 +1,8 @@
 const Hotel = require('../models/hotel')
-const {cloudinary}=require('../cloudinary')
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+const { cloudinary } = require('../cloudinary')
 
 module.exports.index = async (req, res) => {
     const hotels = await Hotel.find({}).populate("Reviews");
@@ -11,10 +14,14 @@ module.exports.renderNewForm = async (req, res) => {
 }
 
 module.exports.createHotelasync = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.hotel.location,
+        limit: 1
+    }).send()
     const body = req.body.hotel;
     const hotel = new Hotel(body);
-    
-    hotel.images=req.files.map(f=>({url:f.path,filename:f.filename}))
+    hotel.geometry = geoData.body.features[0].geometry;
+    hotel.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
     hotel.Owner = req.user._id;
     await hotel.save();
     console.log(hotel);
@@ -57,20 +64,20 @@ module.exports.updateHotel = async (req, res, next) => {
     console.log("------------------------------")
 
     const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { ...req.body.hotel, });
-    const images=req.files.map(f=>({url:f.path,filename:f.filename}))
-    updatedHotel.Rooms=[]
+    const images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    updatedHotel.Rooms = []
     updatedHotel.Reviews = [];
 
     updatedHotel.images.push(...images);
     await updatedHotel.save();
-    if (req.body.dleteImages){
-      for(let filename of req.body.dleteImages){
-        await cloudinary.uploader.destroy(filename)
+    if (req.body.dleteImages) {
+        for (let filename of req.body.dleteImages) {
+            await cloudinary.uploader.destroy(filename)
 
-      }
-      await Hotel.updateOne({
-        $pull: { images: { filename: { $in: req.body.dleteImages } } },
-      });
+        }
+        await Hotel.updateOne({
+            $pull: { images: { filename: { $in: req.body.dleteImages } } },
+        });
     }
     await updatedHotel.save();
 
--- models/hotel.js
+++ models/hotel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const Room = require("./room.js");
 const Review = require("./review.js");
-const User= require("./user.js");
-const schema =mongoose.Schema;
+const User = require("./user.js");
+const schema = mongoose.Schema;
 const { Schema, Types } = mongoose; // Importing Types from mongoose
 
 const ReservationSchema = new Schema({
@@ -10,33 +10,56 @@ const ReservationSchema = new Schema({
   Date: { type: Date },
 });
 
+
+
 const imageSchema = new Schema({
   url: String,
   filename: String,
 
 });
-imageSchema.virtual('thumbnail').get(function(){
-  return this.url.replace('/upload','/upload/w_200')
+imageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_200')
 
 })
+
+
+const opts = { toJSON: { virtuals: true } };
+
 const HotelSchema = new schema({
   name: { type: String, required: true },
   describtion: { type: String, required: true },
   images: [imageSchema],
+  geometry: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
   location: { type: String, required: true },
   Rooms: [{ type: schema.Types.ObjectId, ref: "Room", required: true }],
   Reviews: [{ type: schema.Types.ObjectId, ref: "Review" }],
   Reservations: [{ type: schema.Types.ObjectId, ref: "ReservationSchema" }], // Array of objects with two fields
   Owner: {
-       type: schema.Types.ObjectId,
-       ref: "User" ,
+    type: schema.Types.ObjectId,
+    ref: "User",
   },
+}, opts);
+
+
+HotelSchema.virtual('properties.popUpMarkup').get(function () {
+  return `
+  <strong><a href="/hotels/${this._id}">${this.name}</a><strong>
+  <p>${this.describtion.substring(0, 20)}...</p>`
 });
 
 
-
-HotelSchema.post('findOneAndDelete',async function(hotel){
-  if (hotel.Rooms.length>0) {
+HotelSchema.post('findOneAndDelete', async function (hotel) {
+  if (hotel.Rooms.length > 0) {
     await Room.deleteMany({ _id: { $in: hotel.Rooms } });
   }
   if (hotel.Reviews.length > 0) {
